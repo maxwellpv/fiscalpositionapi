e038a47fff8f9620b90980630fbae9c9ec87c47d
maxwellpv <55470371+maxwellpv@users.noreply.github.com>
2022-12-06 19:33:43 -0500

    Project creation.

---
+++ src/main/java/com/example/fiscalpositionapi/FiscalpositionapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.fiscalpositionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiscalpositionapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiscalpositionapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fiscalpositionapi/FiscalpositionapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fiscalpositionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiscalpositionapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
