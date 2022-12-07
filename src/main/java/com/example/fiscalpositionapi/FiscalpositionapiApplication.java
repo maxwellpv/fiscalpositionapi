ef668876d0ca9d6733627fea29ab2a5fef5f8747
maxwellpv <55470371+maxwellpv@users.noreply.github.com>
2022-12-07 10:30:22 -0500

    Application properties and dependencies configuration.

--- src/main/java/com/example/fiscalpositionapi/FiscalpositionapiApplication.java
+++ src/main/java/com/example/fiscalpositionapi/FiscalpositionapiApplication.java
@@ -1,8 +1,12 @@
 package com.example.fiscalpositionapi;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition
+@EnableJpaAuditing
 @SpringBootApplication
 public class FiscalpositionapiApplication {
 
